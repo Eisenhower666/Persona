2424112045350e49d3a6677d28f411894833acf1
Eisenhower666 <1124612620@qq.com>
2020-11-11 09:08:20 +0800

    first commit

---
+++ src/main/java/com/thoughtworks/xbyi/persona/application/restful/HomeEndPoint.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.xbyi.persona.application.restful;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HomeEndPoint {
+    @GetMapping("/")
+    Map<String, String> home() {
+        Map message = new HashMap<String, String>();
+        message.put("name", "Persona");
+        return message;
+    }
+
+    @GetMapping("/credits")
+    Map<String, String> job() {
+        Map message = new HashMap<String, String>();
+        message.put("job", "dagongren");
+        return message;
+    }
+}
---
+++ src/main/java/com/thoughtworks/xbyi/persona/application/restful/PersonaProfileEndPoint.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.xbyi.persona.application.restful;
+
+import com.thoughtworks.xbyi.persona.domain.entity.Credit;
+import com.thoughtworks.xbyi.persona.domain.entity.Demographic;
+import com.thoughtworks.xbyi.persona.domain.repository.DemographicRepository;
+import com.thoughtworks.xbyi.persona.domain.repository.ICreditRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PersonaProfileEndPoint {
+    @Autowired
+    private DemographicRepository demographicRepository;
+
+    @Autowired
+    private ICreditRepository creditRepository;
+
+    @GetMapping("/demographics")
+    List<Demographic> getDemographics() {
+        return demographicRepository.findAll();
+    }
+
+    @GetMapping("/credit")
+    List<Credit> getCredit() {return creditRepository.findAll();}
+}

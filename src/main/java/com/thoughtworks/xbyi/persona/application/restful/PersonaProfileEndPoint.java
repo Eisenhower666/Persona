926dbdd77caf6644704f0b40980867c9f66fac08
Eisenhower666 <1124612620@qq.com>
2020-11-11 16:05:01 +0800

    feature (#1F004#) create api

--- src/main/java/com/thoughtworks/xbyi/persona/application/restful/PersonaProfileEndPoint.java
+++ src/main/java/com/thoughtworks/xbyi/persona/application/restful/PersonaProfileEndPoint.java
@@ -1,8 +1,10 @@
 package com.thoughtworks.xbyi.persona.application.restful;
 
+import com.thoughtworks.xbyi.persona.domain.entity.ConsumeFeature;
 import com.thoughtworks.xbyi.persona.domain.entity.Credit;
 import com.thoughtworks.xbyi.persona.domain.entity.Demographic;
 import com.thoughtworks.xbyi.persona.domain.repository.DemographicRepository;
+import com.thoughtworks.xbyi.persona.domain.repository.IConsumeRepository;
 import com.thoughtworks.xbyi.persona.domain.repository.ICreditRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,9 @@ public class PersonaProfileEndPoint {
     @Autowired
     private ICreditRepository creditRepository;
 
+    @Autowired
+    private IConsumeRepository consumeRepository;
+
     @GetMapping("/demographics")
     List<Demographic> getDemographics() {
         return demographicRepository.findAll();
@@ -25,4 +30,7 @@ public class PersonaProfileEndPoint {
 
     @GetMapping("/credit")
     List<Credit> getCredit() {return creditRepository.findAll();}
+
+    @GetMapping("/consume")
+    List<ConsumeFeature> getConsume() {return consumeRepository.findAll();}
 }
